04b237f5c680db081af2d2e162a6376f35f76e0d
Devolas <darian.dihal@gmail.com>
2026-02-06 20:22:33 -0500

    added server errors check, added .catch for response errors, fixed container formatting, main function is IIFE

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-import {API_KEY} from "./keys.js";
+import { API_KEY } from "./keys.js";
 
 function fetchImage(searchTerm = "minecraft") {
     let img = document.getElementById("img_main");
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}=${searchTerm}`)
-        .then(function (response) {
-            return response.json();
+        .then(response => {
+            if (!response.ok) { //checks for bad server responses (even if the response is successful) 
+                throw new Error(`HTTP error, status: ${response.status}`);
+            }
+            return response.json(); //parses it to json so that it can be used in the next .then
         })
-        .then(function (response) {
+        .then(response => {
             console.log(response.data.images.original.url);
             img.src = response.data.images.original.url;
+        })
+        .catch(error => { //any errors that happen during the .then are caught by the .catch
+            console.error("Error fetching a gif from the source:", error);
         });
 };
 
-function searchValue(){
+function searchValue() {
     const searchBoxValue = document.getElementById("searchBox")?.value;
     if (searchBoxValue && searchBoxValue.replace(/\s/g, "") !== "") {
         fetchImage(searchBoxValue);
     }
-    else{
+    else {
         fetchImage();
     }
 }
@@ -25,15 +31,12 @@ function searchValue(){
 const container = document.getElementById("container");
 
 const fetchnewImage = document.getElementById("fetchImage");
-fetchnewImage.textContent = "fetch image";
 fetchnewImage.addEventListener('click', () => {
     searchValue();
     console.log("fetchImage button pressed");
 });
 
-container.appendChild(fetchnewImage);
-
-/// run on startup
-fetchImage();
-
-
+/// IIFE run on startup
+(() => {
+    fetchImage();
+})();
